5caea68803a30e9be630bae7ae8552d8d7c96043
Devin <devindford@att.net>
2020-05-14 19:54:21 -0400

    Changed from id to class for button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("#gradient");
-var button = document.querySelector("#randBtn");
+var button = document.querySelector(".randBtn");
 
 function setGradient() {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
