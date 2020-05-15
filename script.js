7e97721134a07fa1518bbd01ea83bbd9deb1bf44
Devin <devindford@att.net>
2020-05-15 02:17:03 -0400

    bgColor refactor to shorter string

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function random_bg_color() {
   var x = Math.floor(Math.random() * 256);
   var y = Math.floor(Math.random() * 256);
   var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  var bgColor = `rgb(${x},${y},${z})`;
   return bgColor;
 }
 
